49dafbf98401cfca84d30aa294657d5aea2df6d7
Gerald Meier <gerald.meier@kistler.com>
2023-06-08 21:30:38 +0200

    fix downloader

--- utils/KaggleCommands/KaggleCommand.py
+++ utils/KaggleCommands/KaggleCommand.py
@@ -9,115 +9,125 @@ from abc import ABC, abstractmethod
 # kaggle kernels {list, init, push, pull, output, status}
 # kaggle config {view, set, unset}
 
-#res= bash('kaggle '+getKaggleEntityString(dataSetType,True)+' download -q -f '+fileName+' -p '+tmpPath+''+(' --unzip' if isZip else'')+' '+dataSetRef)
-#res=bash('kaggle kernels pull '+kernelRef+' -p '+path4currentKernel)
-#currentList = kaggleCommand2DF(getCommandStr(commandStr,page,paginatedType.getPageSize()))
-#command ='kaggle '+getKaggleEntityString(dataSetType,True)+' files '+dataSetRef
-
+# res= bash('kaggle '+getKaggleEntityString(dataSetType,True)+' download -q -f '+fileName+' -p '+tmpPath+''+(' --unzip' if isZip else'')+' '+dataSetRef)
+# res=bash('kaggle kernels pull '+kernelRef+' -p '+path4currentKernel)
+# currentList = kaggleCommand2DF(getCommandStr(commandStr,page,paginatedType.getPageSize()))
+# command ='kaggle '+getKaggleEntityString(dataSetType,True)+' files '+dataSetRef
 
 
 class KaggleCommand():
 
-    INITALPARAMETERS = {"sort-by":None,
-                    "size":None,
-                    "file-type":None,
-                    "license-name":None,
-                    "tag-ids":None,
-                    "search":None,
-                    "user":None,
-                    "mine":False,
-                    "page":1,
-                    "max-size":None,
-                    "min-size":None}
+    INITALPARAMETERS = {"sort-by": None,
+                        "size": None,
+                        "file-type": None,
+                        "license-name": None,
+                        "tag-ids": None,
+                        "search": None,
+                        "user": None,
+                        "mine": False,
+                        "page": 1,
+                        "max-size": None,
+                        "min-size": None}
 
-    def __init__(self, entity, operation, parameters={},entityRef=None):
+    def __init__(self, entity, operation, parameters={}, entityRef=None):
         self._entity = entity
         self._operation = operation
         self._parameters = {**self.INITALPARAMETERS, **parameters}
         self._entityRef = entityRef
-    
+
     @property
     def entity(self):
-            return self._entity
-    
+        return self._entity
+
     @property
     def operation(self):
-            return self._operation
-    
+        return self._operation
+
     @property
     def parameters(self):
-            return self._parameters
-    
+        return self._parameters
+
     @parameters.setter
     def parameters(self, new_parameters):
         self._parameters = {**self._parameters, **new_parameters}
-    
+
     @property
     def entityRef(self):
-            return self._entityRef
+        return self._entityRef
 
     @staticmethod
-    def buildCommand(entity, operation, parameters={},entityRef=None):
-        return KaggleCommand(entity, operation, parameters,entityRef)
-        
-        
-    def execute(self):    
-        if self.entity==KaggleEntityType.DATASET:
-            if(self.operation==KaggleCommandOperations.LIST):
-                #https://github.com/Kaggle/kaggle-api/blob/15cb3f2490db9e0fad4b7662e3621ad949232510/kaggle/api/kaggle_api_extended.py#L828
-                del self._parameters['page-size']
-                del self._parameters['sort-by']
-                del self._parameters['file-type']
-                del self._parameters['license-name']
-                del self._parameters['tag-ids']
-                del self._parameters['max-size']
-                del self._parameters['min-size']
-                #return https://github.com/Kaggle/kaggle-api/blob/15cb3f2490db9e0fad4b7662e3621ad949232510/kaggle/models/kaggle_models_extended.py#L67
+    def buildCommand(entity, operation, parameters={}, entityRef=None):
+        return KaggleCommand(entity, operation, parameters, entityRef)
+
+    def execute(self):
+        if self.entity == KaggleEntityType.DATASET:
+            if (self.operation == KaggleCommandOperations.LIST):
+                # https://github.com/Kaggle/kaggle-api/blob/15cb3f2490db9e0fad4b7662e3621ad949232510/kaggle/api/kaggle_api_extended.py#L828
+                checkAndDeleteProperty(self._parameters, 'page-size')
+                checkAndDeleteProperty(self._parameters, 'sort-by')
+                checkAndDeleteProperty(self._parameters, 'file-type')
+                checkAndDeleteProperty(self._parameters, 'license-name')
+                checkAndDeleteProperty(self._parameters, 'tag-ids')
+                checkAndDeleteProperty(self._parameters, 'max-size')
+                checkAndDeleteProperty(self._parameters, 'min-size')
+                # return https://github.com/Kaggle/kaggle-api/blob/15cb3f2490db9e0fad4b7662e3621ad949232510/kaggle/models/kaggle_models_extended.py#L67
                 return getApi().dataset_list(**self.parameters)
             else:
                 raise CommandTypDoesNotExist()
-        elif self.entity==KaggleEntityType.COMPETITION:
-            raise CommandTypDoesNotExist(str(self.entity)+' '+str(self.operation))
-        elif self.entity==KaggleEntityType.KERNEL:
-            if(self.operation==KaggleCommandOperations.DOWNLOAD):
-                #https://github.com/Kaggle/kaggle-api/blob/15cb3f2490db9e0fad4b7662e3621ad949232510/kaggle/api/kaggle_api_extended.py#L828
-                del self._parameters['sort-by']
-                del self._parameters['size']
-                del self._parameters['file-type']
-                del self._parameters['license-name']
-                del self._parameters['tag-ids']
-                del self._parameters['search']
-                del self._parameters['user']
-                del self._parameters['mine']
-                del self._parameters['page']
-                del self._parameters['max-size']
-                del self._parameters['min-size']
-                path=self._parameters['path']
-                del self._parameters['path']
-                res = getApi().kernel_pull(self.entityRef.split("/")[0],self.entityRef.split("/")[1],**self.parameters)
-                f = open(path+res['metadata']['slug']+'.ipynb', "w",encoding="utf-8")
+        elif self.entity == KaggleEntityType.COMPETITION:
+            raise CommandTypDoesNotExist(
+                str(self.entity)+' '+str(self.operation))
+        elif self.entity == KaggleEntityType.KERNEL:
+            if (self.operation == KaggleCommandOperations.DOWNLOAD):
+                # https://github.com/Kaggle/kaggle-api/blob/15cb3f2490db9e0fad4b7662e3621ad949232510/kaggle/api/kaggle_api_extended.py#L828
+                checkAndDeleteProperty(self._parameters, 'sort-by')
+                checkAndDeleteProperty(self._parameters, 'size')
+                checkAndDeleteProperty(self._parameters, 'file-type')
+                checkAndDeleteProperty(self._parameters, 'license-name')
+                checkAndDeleteProperty(self._parameters, 'tag-ids')
+                checkAndDeleteProperty(self._parameters, 'search')
+                checkAndDeleteProperty(self._parameters, 'user')
+                checkAndDeleteProperty(self._parameters, 'mine')
+                checkAndDeleteProperty(self._parameters, 'page')
+                checkAndDeleteProperty(self._parameters, 'max-size')
+                checkAndDeleteProperty(self._parameters, 'min-size')
+                path = self._parameters['path']
+                checkAndDeleteProperty(self._parameters, 'path')
+
+                res = getApi().kernel_pull(self.entityRef.split(
+                    "/")[0], self.entityRef.split("/")[1], **self.parameters)
+                f = open(path+res['metadata']['slug'] +
+                         '.ipynb', "w", encoding="utf-8")
                 f.write(res['blob']['source'])
                 f.close()
-            elif(self.operation==KaggleCommandOperations.LIST):
-                #https://github.com/Kaggle/kaggle-api/blob/15cb3f2490db9e0fad4b7662e3621ad949232510/kaggle/api/kaggle_api_extended.py#L828
-                del self._parameters['size']
-                del self._parameters['file-type']
-                del self._parameters['license-name']
-                del self._parameters['tag-ids']
-                del self._parameters['max-size']
-                del self._parameters['min-size']
-                del self._parameters['sort-by']
-                del self._parameters['page-size']
+            elif (self.operation == KaggleCommandOperations.LIST):
+                # https://github.com/Kaggle/kaggle-api/blob/15cb3f2490db9e0fad4b7662e3621ad949232510/kaggle/api/kaggle_api_extended.py#L828
+                checkAndDeleteProperty(self._parameters, 'size')
+                checkAndDeleteProperty(self._parameters, 'file-type')
+                checkAndDeleteProperty(self._parameters, 'license-name')
+                checkAndDeleteProperty(self._parameters, 'tag-ids')
+                checkAndDeleteProperty(self._parameters, 'max-size')
+                checkAndDeleteProperty(self._parameters, 'min-size')
+                checkAndDeleteProperty(self._parameters, 'sort-by')
+                checkAndDeleteProperty(self._parameters, 'page-size')
                 return getApi().kernels_list(**self.parameters)
             else:
-                raise CommandTypDoesNotExist(str(self.entity)+' '+str(self.operation))
+                raise CommandTypDoesNotExist(
+                    str(self.entity)+' '+str(self.operation))
         else:
-            raise CommandTypDoesNotExist(str(self.entity)+' '+str(self.operation))
+            raise CommandTypDoesNotExist(
+                str(self.entity)+' '+str(self.operation))
+
+
+def checkAndDeleteProperty(object, property):
+    if property in object:
+        del object[property]
+
 
 class KaggleCommandOperations(Enum):
-  LIST = 1
-  files = 2
-  DOWNLOAD = 3
-  submit = 4
-  submissions = 5
-  leaderboard = 6
+    LIST = 1
+    files = 2
+    DOWNLOAD = 3
+    submit = 4
+    submissions = 5
+    leaderboard = 6
